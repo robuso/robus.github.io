00faeb2eabdaeb1b1dde623c7784e519467af0a5
Olin Robus <olin.robus@gmail.com>
2024-03-15 11:09:02 -0500

    redo of the SR stuff

--- build.py
+++ build.py
@@ -2,34 +2,24 @@ import os
 import time
 import subprocess
 import sqlite3
-
-# from watchdog.observers import Observer
-# from watchdog.events import FileSystemEventHandler
-
-
-# class MyHandler(FileSystemEventHandler):
-#     def __init__(self, conn, output_directory):
-#         super().__init__()
-#         self.conn = conn
-#         self.created_files = set()
-#         self.output_directory = output_directory
-
-#     def on_created(self, event):
-#         if event.is_directory:
-#             return
-#         self.created_files.add(event.src_path)
+import hashlib
 
 
 def create_database(conn):
     cursor = conn.cursor()
     cursor.execute(
-        """CREATE TABLE IF NOT EXISTS files
-                    (path TEXT PRIMARY KEY)"""
+        """CREATE TABLE IF NOT EXISTS files (path TEXT PRIMARY KEY, hash TEXT)"""
     )
     conn.commit()
 
 
-def convert_with_pandoc(input_file, output_file, css_file, js_file):
+def get_file_hash(file_path):
+    with open(file_path, "rb") as f:
+        file_hash = hashlib.blake2b(f.read()).hexdigest()
+    return file_hash
+
+
+def convert_with_pandoc(input_file, output_file, css_file, js_file, header):
     try:
         subprocess.run(
             [
@@ -42,6 +32,10 @@ def convert_with_pandoc(input_file, output_file, css_file, js_file):
                 "--include-in-header",
                 js_file,
                 "--katex",
+                "-B", # "--include-before-body="
+                header,
+
+                
             ],
             check=True,
         )
@@ -57,6 +51,7 @@ def main():
     css_file = "/style.css"
     js_file = "/Users/orobus/Documents/dev/robuso.github.io/script.js"
     database_file = "/Users/orobus/Documents/dev/robuso.github.io/files.db"
+    header = "header.html"
 
     # Connect to the database
     conn = sqlite3.connect(database_file)
@@ -64,24 +59,27 @@ def main():
 
     # Load state from the database
     cursor = conn.cursor()
-    cursor.execute("SELECT path FROM files")
-    created_files = {row[0] for row in cursor.fetchall()}
+    cursor.execute("SELECT path, hash FROM files")
+    created_files = {row[0]: row[1] for row in cursor.fetchall()}
 
-    # Initialize the event handler
-    # event_handler = MyHandler(conn, output_directory)
-
-    # Check for newly created files and convert them
     for root, _, files in os.walk(directory_path):
         for file in files:
             if file.endswith(".md"):
                 file_path = os.path.join(root, file)
-                if file_path not in created_files:
+                file_hash = get_file_hash(file_path)
+                if (
+                    file_path not in created_files
+                    or created_files[file_path] != file_hash
+                ):
                     output_file = os.path.join(
                         output_directory, file.replace(".md", ".html")
                     )
-                    convert_with_pandoc(file_path, output_file, css_file, js_file)
-                    # Add the file to the database
-                    cursor.execute("INSERT INTO files (path) VALUES (?)", (file_path,))
+                    convert_with_pandoc(file_path, output_file, css_file, js_file, header)
+                    # Update or insert the file in the database
+                    cursor.execute(
+                        "REPLACE INTO files (path, hash) VALUES (?, ?)",
+                        (file_path, file_hash),
+                    )
                     conn.commit()
 
     conn.close()
